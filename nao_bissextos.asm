ee1c176d9c5e252cf8e5ef5f109314eef67e4376
Pedro <46246172+peperone9@users.noreply.github.com>
2026-05-14 10:47:25 -0300

    Add files via upload

--- nao_bissextos.asm
+++ nao_bissextos.asm
@@ -1,8 +1,9 @@
 .data
-	msg1: .asciiz "primeiro valor: "
-	msg2: .asciiz "segundo valor: "
+	msg1: .asciiz "\nprimeiro valor: "
+	msg2: .asciiz "\nsegundo valor: "
 	erro1: .asciiz "== A DIFERENCA ENTRE OS ANOS PRECISA SER MENOR QUE 1000 ==\n\n"
 	erro2: .asciiz "== OS ANOS PRECISAM SER VALORES INTEIROS POSITIVOS ==\n\n"
+	msg3: .asciiz " anos nao bissextos entre os valores\n"
 .text
 
 .globl main
@@ -25,11 +26,16 @@ main:
 	#le segundo valor
 	li $v0, 5
 	syscall
+	
+	#valida entradas
 	blt $v0, 0, anonegativo
+	
 	#valida diferença entre os anos
 	sub $t2, $t1, $v0
 	abs $t2, $t2
 	bgt $t2, 1000, errodediferenca
+	
+	beq $t2, $zero, fim
 	j calculo	
 	
 #erro caso a diferença seja maior que 1000 anos
@@ -45,10 +51,50 @@ anonegativo:
 	la $a0, erro2
 	syscall
 	j main
-
+    
 calculo:
 	#ordena valores:
 	
 	#maior
-	
-	
+    add $t3, $t1, $v0
+    sub $t4, $t1, $v0
+    abs $t4, $t4
+    add $t3, $t3, $t4
+    div $t3, $t3, 2 
+    
+    #MENOR
+    add $t4, $t1, $v0
+    sub $t5, $t1, $v0
+    abs $t5, $t5
+    sub $t4, $t4, $t5
+    div $t4, $t4, 2 
+    
+    #Exclui o ano inicial do calculo
+    add $t4, $t4, 1
+    
+    loop:
+    rem $t6, $t4, 400 #valida divisibilidade por 400
+    beq $t6, $zero, bissexto
+    rem $t6, $t6, 100
+    beq $t6, $zero, fimbissexto #sai da condicao caso seja divisivel por 100
+    rem $t6, $t6, 4
+    beq $t6, $zero, bissexto
+    
+    fimbissexto:
+    add $t4,$t4, 1
+    blt $t4, $t3, loop
+    j fim
+    
+    
+bissexto:
+     sub $t2, $t2, 1 
+     j fimbissexto
+     
+fim:
+    li $v0, 1 
+    add $a0, $t2, 0
+    syscall
+    
+    li $v0, 4
+    la $a0, msg3
+    syscall
